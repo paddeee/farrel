d6a60d57184bd7695c3fb5b907e2a451a0740d5a
paddeee <paddeee@googlemail.com>
2016-09-20 14:40:22 +0100

    Re-added code to select all and deselect all checkboxes when filters are cleared

--- app/scripts/stores/queryBuilder.js
+++ app/scripts/stores/queryBuilder.js
@@ -29,6 +29,9 @@ module.exports = Reflux.createStore({
 
     // Used to prevent Checkboxes being displayed when none of the filters have values
     this.filtersWithValues = [];
+
+    // Does the Query contain any events filters with values set by the user
+    this.containsEvents = false;
   },
 
   // Set the filteredData Object
@@ -234,6 +237,8 @@ module.exports = Reflux.createStore({
     } else if (action === 'remove') {
       this.removeFilterWithValues(filter);
     }
+
+    this.manageContainsEventFilters();
   },
 
   // See whether to add an object to filtersWithValues
@@ -287,5 +292,24 @@ module.exports = Reflux.createStore({
   resetFiltersWithValues: function() {
 
     this.filtersWithValues = [];
+
+    this.manageContainsEventFilters();
+  },
+
+  // Manage property to indicate if there are any event filters. Because if not, the application shouldn't select all
+  // event checkboxes if a filter is added to a different data type. Otherwise a filter on people will select all
+  // events which will select more people which would be confusing to user.
+  manageContainsEventFilters: function() {
+
+    var containsEvents = false;
+
+    this.filtersWithValues.forEach(function(filter) {
+
+      if (filter.collectionName === config.EventsCollection.name) {
+        containsEvents = true;
+      }
+    });
+
+    this.containsEvents = containsEvents;
   }
 });
